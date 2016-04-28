f51df6febbf1153bf9874f044b71014840485003
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2016-04-28 18:11:27 -0400

    slightly better deprecation for Capfile

--- lib/capistrano/delayed-job.rb
+++ lib/capistrano/delayed-job.rb
@@ -1,2 +1,2 @@
-warn %{"require 'capistrano/delayed-job'" is deprecated; please use "require 'capistrano/delayed_job'" in #{Kernel.caller[1]}}
+warn %{"require 'capistrano/delayed-job'" is deprecated; please use "require 'capistrano/delayed_job'" in #{Kernel.caller(1,1).first}}
 require_relative 'delayed_job'
